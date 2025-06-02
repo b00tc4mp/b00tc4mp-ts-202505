9f44f0dce7d875035edf32987c8159f5464c91dd
Eduardolans <141261251+Eduardolans@users.noreply.github.com>
2025-06-02 14:51:31 +0200

    add product api package with user model and populate #8

---
+++ staff/eduardo-criado/.zzz/May27/models 1.ts
@@ -0,0 +1,32 @@
+import { ObjectId, Schema, model } from "mongoose";
+
+export const user = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+
+  avatar: {
+    type: String,
+    required: true,
+  },
+
+  username: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = model("User", user);
+
+User.create();
---
+++ staff/eduardo-criado/product/api/data/types.ts
@@ -0,0 +1,7 @@
+export interface IUserDoc {
+  name: string;
+  email: string;
+  avatar: string;
+  username: string;
+  password: string;
+}
