7d72f899f1e537a39fb77a9fe91d695ece92e511
manuelbarzi <manuelbarzi@gmail.com>
2025-07-02 21:42:03 +0200

    add perplexity demo #5

---
+++ stuff/perplexity/test.sh
@@ -0,0 +1,17 @@
+curl --location 'https://api.perplexity.ai/chat/completions' \
+--header 'accept: application/json' \
+--header 'content-type: application/json' \
+--header 'Authorization: Bearer API_KEY' \
+--data '{
+  "model": "sonar-pro",
+  "messages": [
+    {
+      "role": "system",
+      "content": "Be precise and concise."
+    },
+    {
+      "role": "user",
+      "content": "How many stars are there in our galaxy?"
+    }
+  ]
+}'
